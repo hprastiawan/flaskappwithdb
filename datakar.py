466df06acf8426dd72dec53f726d80a4013939c0
Hendra Prastiawan <93511143+hprastiawan@users.noreply.github.com>
2023-09-26 12:16:47 +0700

    Update datakar.py

--- datakar.py
+++ datakar.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 # Konfigurasi database
 app.config['SQLALCHEMY_ECH0'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:m1B14fwDsBDtwMYS7x12@containers-us-west-167.railway.app:7090/railway'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:m1B14fwDsBDtwMYS7x12@containers-us-west-167.railway.app:7090/railway'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Konfigurasi Swagger
